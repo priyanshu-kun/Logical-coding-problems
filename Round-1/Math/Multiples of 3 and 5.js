35ac6ad0fd1124d154f751ac35f8886efb064aaf
priyanshuSharma-WebDev <priyanshushrama709@gmail.com>
2021-04-12 16:27:36 +0530

    fix multiples of 3 and 5

--- Round-1/Math/Multiples of 3 and 5.js
+++ Round-1/Math/Multiples of 3 and 5.js
@@ -13,10 +13,10 @@ const Find_Addition_of_Multiples = (three, five, limit) => {
 const More_Efficient_Sol_Find_Addition_of_Multiples = (three, five, limit) => {
     function sumDivisibleByN(N) {
         // Get the multiples of 5 OR 3 between 1 -> limit(1000) - let's take a example, we get no of multiples is 333.3333333.... so on and if we round it to the nearst integer then we get 333.
-        let p = Math.round(limit / N);
+        let p = Math.floor((limit - 1) / N);
         // Formula to get sum of all posible mutiples between 1 -> limit
         // General classification - 3+6+9+12+......+999 = 3*(1+2+3+4+...+333)
-        return Math.round(N * (p * (p + 1)) / 2);
+        return Math.floor(N * (p * (p + 1) / 2));
     }
     /**
      * To get a more efficient solution we could also calculate the sum of the numbers less
@@ -26,5 +26,5 @@ const More_Efficient_Sol_Find_Addition_of_Multiples = (three, five, limit) => {
      */
     return sumDivisibleByN(3) + sumDivisibleByN(5) - sumDivisibleByN(15);
 }
-console.log(More_Efficient_Sol_Find_Addition_of_Multiples(3, 5, 1000000000))
+console.log(More_Efficient_Sol_Find_Addition_of_Multiples(3, 5, 1000))
 // console.log(Find_Addition_of_Multiples(3, 5, 1000));
