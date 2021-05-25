30703a4b4309547f13d6bb3646fef4f1fdde8967
priyanshuSharma-WebDev <priyanshushrama709@gmail.com>
2021-05-25 19:29:02 +0530

    implement linkedlist

--- Round-1/LinkedList/Implementation.cpp
+++ Round-1/LinkedList/Implementation.cpp
@@ -1,64 +1,52 @@
 #include <iostream>
 using namespace std;
 #define ll long long int
-const ll mod=1e9+7;
-#define take_input freopen("input.txt","r",stdin),freopen("output.txt","w",stdout);
-#define fast ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
 
 class Linked_List {
     public:
-        // int this variable we store data for linkedlist 
         ll data;
-        // temp is a pointer who store memory address of next node of Linked_List type 
-        Linked_List *next;
-        // for set default value of pointer
-        Linked_List() {
-            this->next = NULL;
+        Linked_List* next;
+    Linked_List() {
+        this->next = NULL;
+    }
+    Linked_List(ll val) {
+        this->data = val;
+        this->next = NULL;
+    }
+    Linked_List* addNode(ll val,Linked_List * head) {
+        if(head == NULL) {
+            head = new Linked_List(val);
         }
-        // setting data and null part in node
-        Linked_List(ll data) {
-            this->data = data;
-            this->next = NULL;
-        }
-        Linked_List *add(ll data,Linked_List *head) {
-            // CASE: 1 - if there is no head in linkedlist
-            if(head == NULL) {
-                Linked_List *temp = new Linked_List(data);
-                head = temp;
+        else {
+            Linked_List * temp = head;
+            while (temp->next != NULL) {
+                temp = temp->next;
             }
-            else {
-                // CASE: 2 if linkedlist has head then add new node onto the edge of linkedlist
-                Linked_List *temp = head;
-                while(temp->next != NULL) {
-                    temp = temp->next;
-                    temp->next = new Linked_List(data);
-                }
-            }
-            return head;
+            temp->next = new Linked_List(val);
         }
-        void print(Linked_List *head) {
-            auto temp = head;
-            while(temp != NULL) {
-                cout << "BOOM"<< endl;
-                cout << temp->data << " ";
-                temp=temp->next;
-            }
-            cout << endl;
-        }
+        return head;
+    }
 
+    void print(Linked_List *head) {
+        Linked_List * temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data * 2 << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
 };
 
+
 int main() {
-    // cout << "BOOM";
-    // take_input;
-    // fast;
-    Linked_List *head = NULL;
-    head=head->add(1,head);
-    head=head->add(2,head);
-    head=head->add(3,head);
-    head=head->add(4,head);
-    head=head->add(5,head);
-    head=head->add(6,head);
+    
+    Linked_List * head = NULL;
+    ll i = 1;
+    while (i <= 10) {
+        head = head->addNode(i,head);   
+        i++;
+    }
     head->print(head);
     return 0;
 }
