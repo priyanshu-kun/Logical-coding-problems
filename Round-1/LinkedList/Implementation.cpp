cda3cc74d1f7e7105c37929846015235b0cae028
Kira <priyanshushrama709@gmail.com>
2021-05-30 18:51:21 +0530

    find middle of linkedlist

--- Round-1/LinkedList/Implementation.cpp
+++ Round-1/LinkedList/Implementation.cpp
@@ -4,16 +4,19 @@ using namespace std;
 
 class Linked_List {
     public:
+        Linked_List * prev;
         ll data;
         Linked_List* next;
     Linked_List() {
+        this->prev = NULL;
         this->next = NULL;
     }
     Linked_List(ll val) {
+        this->prev = NULL;
         this->data = val;
         this->next = NULL;
     }
-    Linked_List* addNode(ll val,Linked_List * head) {
+    void addNode(ll val,Linked_List * &head) {
         if(head == NULL) {
             head = new Linked_List(val);
         }
@@ -23,18 +26,48 @@ class Linked_List {
                 temp = temp->next;
             }
             temp->next = new Linked_List(val);
+            temp->next->prev = temp;
         }
-        return head;
+       
     }
 
-    void print(Linked_List *head) {
+    void printForward(Linked_List *head) {
         Linked_List * temp = head;
         while (temp != NULL)
         {
-            cout << temp->data * 2 << " ";
+            cout << temp->data << " ";
             temp = temp->next;
         }
         cout << endl;
+    }
+    void printBackward(Linked_List *tail) {
+        Linked_List * temp = tail;
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->prev;
+        }
+        cout << endl;
+    }
+
+    Linked_List * getTail(Linked_List *head) {
+        Linked_List * temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        return temp;
+    }
+
+// slow and fast pointer for get middle of linkedlist
+    Linked_List * findMiddleOfLinkedList(Linked_List *head) {
+        Linked_List * slow = head;
+        Linked_List * fast = head;
+        while(fast != NULL and fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
     }
 };
 
@@ -43,10 +76,14 @@ int main() {
     
     Linked_List * head = NULL;
     ll i = 1;
-    while (i <= 10) {
-        head = head->addNode(i,head);   
+    while (i <= 5) {
+        head->addNode(i,head);   
         i++;
     }
-    head->print(head);
+    head->printForward(head);
+    auto tail = head->getTail(head);
+    head->printBackward(tail);
+    auto middle = head->findMiddleOfLinkedList(head);
+    cout << "Middle: " << middle->data << endl;
     return 0;
 }
